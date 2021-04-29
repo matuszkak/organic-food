17524e4eb514ad04236484f456453528ec1b5c90
Kristof Matuszka <matuszkak@gmail.com>
2021-04-29 09:26:06 +0200

    responsive menu kész

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ const swiper = new Swiper('.swiper-container', {
 var scroll = new SmoothScroll('a[href*="#"]');
 
 // menu sötétítés lefelé törtlnő scrollozáskor
+
+
 window.addEventListener('scroll', function () {
   console.log(window.scrollY);
+
   if (window.scrollY >= 147) {
     document.querySelector('.upper-nav-bar').classList.add('darken1');
     document.querySelector('.lower-nav-bar').classList.add('darken2');
@@ -30,23 +33,21 @@ window.addEventListener('scroll', function () {
   }
 });
 
+//scrollozásra eltűnik a függőleges menu
+
+window.addEventListener('scroll', function () {
+  console.log('scrolled!');
+  if (document.querySelector('.menu-container').classList.contains('js-show')) {
+    document.querySelector('.menu-container').classList.remove('js-show');
+    document.querySelector('.menu-container').classList.add('js-hide');
+  }
+});
+
+// hambi menu klikkre előjön a függőleges menu
+
 document.querySelector('.hambi').addEventListener('click', function () {
   console.log('clicked!');
 
   document.querySelector('.menu-container').classList.add('js-show');
 });
 
-
-// var desiredElement = document.querySelectorAll('.hambi');
-// clicked = true;
-// function switchTheme() {
-//   for (var i = 0; i < desiredElement.length; i++) {
-//     if (desiredElement[i].classList.contains('js-show')) {
-//       desiredElement[i].classList.remove('js-show');
-//       desiredElement[i].classList.add('js-hide');
-//     } else if (desiredElement[i].classList.contains('js-hide')) {
-//       desiredElement[i].classList.remove('js-hide');
-//       desiredElement[i].classList.add('js-show');
-//     }
-//   }
-// }
